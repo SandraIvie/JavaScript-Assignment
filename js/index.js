7f1706186c5e7be4db3d7f0f8b9956979eb32fea
Sandraivie <61964245+SandraIvie@users.noreply.github.com>
2020-03-26 20:42:19 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var myName = "sandra Ivienagbor"
+
+
+var myCourses = ["Html" , "css" , "Javascript"]   
+
+alert("My name is" + " " + myName + " " + "and I offer" + " " + "" + myCourses);  
+
+
+
+
+for(i=1;i<=200;i++)
+{
+  if(i % 2>0)continue;
+  console.log(i);
+}
